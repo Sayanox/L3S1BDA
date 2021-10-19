9060597a94e0b2f3b15bbc74aba43a6816d314c6
Gekimon12 <76741554+Gekimon12@users.noreply.github.com>
2021-10-19 11:56:11 +0200

    Ajout méthode Init() et un setter sur Catalog.java
     Il faudra sans doute corriger et/ou optimiser Init()

--- MiniSGBDKeryann/src/gdisk/Catalog.java
+++ MiniSGBDKeryann/src/gdisk/Catalog.java
@@ -20,6 +20,29 @@ public class Catalog implements Serializable {
 		
 	}
 	
+	public void Init() throws IOException {
+		String n, nc, type;
+		int nb;
+		final String ESPACE = " ";
+		
+		FileReader fr = new FileReader("/DB/Catalog.def");
+		BufferedReader br = new BufferedReader (fr);
+		StringBuffer sb = new StringBuffer ();
+		String line;
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+			sb.append("/n");
+		}
+		String params[] = (sb.toString()).split(ESPACE);
+		n = params[0];
+		nb = Integer.parseInt(params[1]);
+		nc = params[2];
+		type = params[3];
+		this.AddRelation(new RelationInfo(n, nb, nc, type));
+		this.setComptRelations(Integer.parseInt(params[4]));
+		
+	}
+	
 	public void Finish() throws IOException {
 		File fichier = new File("/DB/Catalog.def");
 		OutputStream os = new FileOutputStream(fichier);
@@ -36,6 +59,11 @@ public class Catalog implements Serializable {
 		listesRelations.add(relation);
 		comptRelations++;
 	}
+	
+	public void setComptRelations(int i) {
+		this.comptRelations = i;
+	}
+
 	
 	public static Catalog getInstance() {
 		return INSTANCE;
